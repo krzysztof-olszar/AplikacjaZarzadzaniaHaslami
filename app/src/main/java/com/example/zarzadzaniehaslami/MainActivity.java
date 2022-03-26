fb12ec2a531ec9d2197a473d19296a310003f7c1
Krios135 <Krios13@onet.pl>
2022-03-26 17:20:00 +0100

    Stacja druga - wczytywanie hasła z pliku

--- app/src/main/java/com/example/zarzadzaniehaslami/MainActivity.java
+++ app/src/main/java/com/example/zarzadzaniehaslami/MainActivity.java
@@ -2,10 +2,22 @@ package com.example.zarzadzaniehaslami;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +25,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        File file = new File(MainActivity.this.getFilesDir(), "dane");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        try {
+            File MainHaslo = new File(file, "MainHaslo.txt");
+            if(!MainHaslo.exists()) {
+                FileWriter writer = new FileWriter(MainHaslo);
+                writer.write("Zosia");
+                writer.flush();
+                writer.close();
+            }
+            //Toast.makeText(MainActivity.this, "Saved your text", Toast.LENGTH_LONG).show();
+        } catch (Exception e) { }
     }
+
 
     public void Zaloguj(View view){
         TextView hasloOkno = (TextView)findViewById(R.id.MainPassword);
-        //TextView textview = (TextView) view;//klikniety przycisk
-        String haslo=hasloOkno.getText().toString();
-        if(haslo.equals("Zosia")){
-            Intent intent = new Intent(this, MainMenu.class);
-            startActivity(intent);
-        }
+        String wpisaneHaslo=hasloOkno.getText().toString();
+        File file = new File(MainActivity.this.getFilesDir(), "dane/MainHaslo.txt");
+        try{
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String haslo = bufferedReader.readLine();
+            if(wpisaneHaslo.equals(haslo)){
+                Intent intent = new Intent(this, MainMenu.class);
+                startActivity(intent);
+            }
+        }catch (Exception e) { }
     }
+
 }
